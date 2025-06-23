49d942e649d8c223e4c6de7fcd62e4fe1e1395e9
zl <1138108272@qq.com>
2025-06-23 10:44:57 +0800

    add:使用微信公众配置weixin-java-mp  进行验证服务

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,32 +2,40 @@ package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.service.WeChatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * index控制器
  */
 @Controller
-
 public class IndexController {
-  @Autowired
-  private WeChatService weChatService;
+    @Autowired
+    private WeChatService weChatService;
 
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "index";
-  }
+    @Value("${wechat.appid}")
+    private String appid;
+    @Value("${wechat.secret}")
+    private String secret;
+
+    /**
+     * 主页页面
+     *
+     * @return API response html
+     */
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 
 
-  @GetMapping("/getToken")
-  public String getToken() {
-    String token = weChatService.getAccessToken("wx129306a8a645f557", "ae4bbc435fd1adab609edc2143485e01");
-    return token;
-  }
+    @GetMapping("/getToken")
+    @ResponseBody
+    public String getToken() {
+        String token = weChatService.getAccessToken(appid, secret);
+        return token;
+    }
 
 }
